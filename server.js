2cc10662baa907771319239949f95e3af5ee8d0d
Tyson Mbonambi <tysonmbonambi@gmail.com>
2020-04-13 12:39:42 +0200

    initial Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+// setup template engine
+app.engine('handlebars', exphbs( {
+    defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+//setup express static public folder for css and js and images
+app.use(express.static('public'));
+//setup environment variables
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log(`Server is Running on port ${port}`);
+});
