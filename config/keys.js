020d70ac196531214f0a7210c3100889cce4fa94
Tyson Mbonambi <tysonmbonambi@gmail.com>
2020-04-13 22:34:42 +0200

    database created

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://Tyson1987:stokoloko@armbrella-3cnxv.mongodb.net/personalwebsite?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const bodyPaser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(keys.MongoURI)
+.then(() => {
+    console.log('Remote MongoDB is connected')
+})
 const app = express();
 // setup template engine
 app.engine('handlebars', exphbs( {
@@ -9,6 +15,24 @@ app.engine('handlebars', exphbs( {
 app.set('view engine', 'handlebars');
 //setup express static public folder for css and js and images
 app.use(express.static('public'));
+//setup body parser to encode url
+app.use(bodyPaser.urlencoded({
+    extended: false
+}));
+app.use(bodyPaser.json());
+// create collection with mangoose
+const Schema = mongoose.Schema;
+const Message = mongoose.model('message', new Schema({
+    name:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+    message:{
+        type:String
+    }
+}));
 //setup environment variables
 const port = process.env.PORT || 3000;
 
@@ -23,6 +47,28 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
+app.post('/getMessage', (req, res) => {
+    const newMessage ={
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    }
+    new Message(newMessage).save()
+    .then(() => {
+        res.render('inbox');
+    })
+});
+app.get('/displayMessage', (req, res) => {
+    Message.find({}, (err, messages) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.render('displayMessage', {
+                messages:messages
+            })
+        }
+    })
+})
 //this is portforlio rout handler
 app.get('/portfolio', (req, res) => {
     res.render('portfolio');
